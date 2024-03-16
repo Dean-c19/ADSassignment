79ac79bf04673f9db4681b1724b9a228f471f679
Dean-c19 <148439940+Dean-c19@users.noreply.github.com>
2024-03-16 20:01:51 +0000

    added in code for AddACar and ViewAllCars also added two methods one to count the cars in showroom and one to check if registration is unique

--- src/main.c
+++ src/main.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+//Preprocessor Variable
+#define MAX_CARS 5
+
+
+//Stucture for car part of the linked list
+struct Car{
+    char registration[8];
+    char makeAndModel[50];
+    char colour[20];
+    int previousOwners;
+    bool reserved;
+    float reservedAmount;
+    struct Car* next;
+
+
+};
 
 
 //function prototypes
-void menu();
-void AddACar();
-void SellACar();
-void ReserveOrUnreserveACar();
-void ViewAllCars();
-void ViewSpecificCar();
-void myTask();
-void ExitTheSystem();
+void menu(struct Car** front);
+void AddACar(struct Car** front);
+void SellACar(struct Car** front);
+void ReserveOrUnreserveACar(struct Car** front);
+void ViewAllCars(struct Car** front);
+void ViewSpecificCar(struct Car** front);
+void myTask(struct Car** front);
+void ExitTheSystem(struct Car** front);
+int carCounter(struct Car* front);
+bool isUniqueRegistration(struct Car* front, const char* registration);
 
 
 
 int main()
 {
-    
-    void menu();
+    struct Car* front = NULL;
+     menu(&front);
     
     return 0;
     }
 
-void menu(){
+void menu(struct Car** front){
  int ans;
     
+    do {
     printf("Menu:\n");
     printf("1. Add a new car to the showroom\n");
     printf("2. Sell a car from the showroom\n");
@@ -37,66 +60,143 @@ void menu(){
     switch(ans)
     {
         case 1:
-            void AddACar();
+            AddACar(front);
             break;
         case 2:
-            void SellACar();
+             SellACar(front);
             break;
         case 3:
-            void ReserveOrUnreserveACar();
+             ReserveOrUnreserveACar(front);
             break;
             case 4:
-          void ViewAllCars();
+           ViewAllCars(front);
             break;
         case 5:
-            void ViewSpecificCar();
+             ViewSpecificCar(front);
             break;
         case 6:
-            void myTask();
+             myTask(front);
             break;
             case 7:
-            void ExitTheSystem();
+             ExitTheSystem(front);
             break;
         default:
             printf("Error, please pick an option between 1 and 7\n");
             break;
     }
-    return 0;
+   
+}while (ans != 7);// do while to loop through the menuu until the 7 is inputted to exti the porgram
+    
+
 }
-    
-    
-void AddACar()
+// Code for adding a new car to the showroom
+void AddACar(struct Car** front)
 {
-    // Code for adding a new car to the showroom
+    // create space for a new car
+    struct Car* newCar = (struct Car*)malloc(sizeof(struct Car));
+//checks if the showroom is full
+    if (carCounter(*front) >= MAX_CARS) {
+        printf("Error Showroom full\n");
+        return;
+    }
+ 
+
+    printf("Input the cars registration in the format yyDxnnnn): \n");
+    scanf("%s", newCar->registration);
+    printf("Input car make and model: \n");
+    scanf("%s", newCar->makeAndModel);
+    printf("Input car colour: \n");
+    scanf("%s", newCar->colour);
+    printf("Input the amount of previous owners: \n");
+    scanf("%d", &newCar->previousOwners);
+    if (newCar->previousOwners > 3) {
+        printf("Error the amount of previous owners cant be more than 3\n");
+        free(newCar); // gets rid of the memory allocated for that car 
+        return;
+    }
+    printf("Is this car reserved please input 1 for yes 0 for no: \n");
+    scanf("%d", &newCar->reserved);
+    if (newCar->reserved) {
+        printf("Input reserved amount: \n");
+        scanf("%f", &newCar->reservedAmount);
+    } else {
+        newCar->reservedAmount = 0;
+    }
+
+    newCar->next = *front;
+    *front = newCar;
+    
+
+   
+
+
+
 }
 
-void SellACar()
+void SellACar(struct Car** front)
 {
     // Code for selling a car from the showroom
 }
 
-void ReserveOrUnreserveACar()
+void ReserveOrUnreserveACar(struct Car** front)
 {
     // Code for reserving/unreserving a car in the showroom
 }
 
-void ViewAllCars()
+void ViewAllCars(struct Car** front)
 {
     // Code for viewing all cars in the showroom
+    printf("\nCars in the showroom:\n");
+    struct Car* current = *front; // start at the front of the list
+    while (current != NULL) {
+        printf("Registration: %s\n", current->registration);
+        printf("Make and Model: %s\n", current->makeAndModel);
+        printf("Colour: %s\n", current->colour);
+        printf("Previous Owners: %d\n", current->previousOwners);
+        printf("Reserved: %s\n", current->reserved ? "Yes" : "No");
+        if (current->reserved) {
+            printf("Reserve Amount: %.2f\n", current->reservedAmount);
+        }
+        printf("\n");
+        current = current->next;
+    }
 }
 
-void ViewSpecificCar()
+void ViewSpecificCar(struct Car** front)
 {
     // Code for viewing a specific car in the showroom
 }
 
-void myTask()
+void myTask(struct Car** front)
 {
     // Code for my task 
 }
 
-void ExitTheSystem()
+void ExitTheSystem(struct Car** front)
 {
     // Code for exiting the system
+    printf("Bye!!\n");
+    exit(0);
 }
-    
+    //method to count the cars in the showroom
+int carCounter(struct Car* front) {
+    int count = 0;
+    struct Car* current = front;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+//methid to check if the registration is unique
+bool isUniqueRegistration(struct Car* front, const char* registration) {
+    struct Car* current = front;
+    while (current != NULL) {
+        if (strcmp(current->registration, registration) == 0) {
+            return false; // returns false if registration already exists
+        }
+        current = current->next;
+    }
+    return true; // returns true if registration is unique
+}
